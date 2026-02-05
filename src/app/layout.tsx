0f7d03ac177cc4367ae95d15647e410e1fda9888
lifestylehomedecor51-ux <urbanrural498@gmail.com>
2026-02-05 12:55:07 +0530

    Changes in emi calculator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Footer } from "./components/Footer";
+import { LoanModalProvider } from "@/components/LoanModalProvider";
 
 
 const geistSans = Geist({
@@ -30,9 +31,11 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} bg-white antialiased`}
             >
-                <Navbar />
-                {children}
-                <Footer/>
+                <LoanModalProvider>
+                    <Navbar />
+                    {children}
+                    <Footer />
+                </LoanModalProvider>
             </body>
         </html>
     );
